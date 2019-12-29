2c500b14d81c3ac60f15055debc6a24f536e374f
Graham Ollis <plicease@cpan.org>
2019-12-29 15:37:30 -0700

    convert into a struct for easier testing

--- src/which/main.rs
+++ src/which/main.rs
@@ -6,98 +6,122 @@ use std::process::exit;
 use std::ffi::OsString;
 use getopts::Options;
 
-#[cfg(windows)]
-fn get_path(skip_dot: bool) -> OsString {
-    if !skip_dot {
-        let mut path = OsString::new();
-        path.push(current_dir().unwrap().as_os_str());
-        path.push(";");
-        path.push(var_os("PATH").unwrap());
-        return path
-    } else {
-        return var_os("PATH").unwrap()
-    }
+struct App {
+    program_name: String,
+    skip_dot: bool,
+    command_vec: Vec<String>
 }
 
-#[cfg(unix)]
-fn get_path(_skip_dot: bool) -> OsString {
-    var_os("PATH").unwrap()
-}
+impl App {
 
-fn print_usage(program: &str, opts: Options, message: Option<getopts::Fail>) -> ! {
-    let brief = format!("Usage: {} [options] COMMAND", program);
-
-    match message {
-        Some(message) => {
-            eprintln!("{}: {}", program, message);
-            eprint!("{}", opts.usage(&brief));
-            exit(1);
-        },
-        None => {
-            print!("{}", opts.usage(&brief));
-            exit(0);
+    fn new() -> App {
+        App {
+            program_name: String::from(""),
+            skip_dot: false,
+            command_vec: [].to_vec(),
         }
     }
-}
 
-fn get_options() -> getopts::Matches {
-    let args: Vec<String> = args().collect();
-    let program = args[0].clone();
-
-    let mut opts = Options::new();
-
-    /* TODO: implement all */
-    opts.optflag("a","all","Print all matches in PATH, not just the first.");
-    opts.optflag("","help", "Print this help and exit successfully.");
     #[cfg(windows)]
-    opts.optflag("","skip-dot", "Do not search current directory (a la PowerShell)");
-    opts.optflag("v","version", "Print version and exit successfully.");
-
-    let matches = match opts.parse(&args[1..]) {
-        Ok(m)  => m,
-        Err(f) => print_usage(&program, opts, Some(f))
-    };
-
-    if matches.opt_present("help") {
-        print_usage(&program, opts, None);
+    fn get_path(&self) -> OsString {
+        if !self.skip_dot {
+            let mut path = OsString::new();
+            path.push(current_dir().unwrap().as_os_str());
+            path.push(";");
+            path.push(var_os("PATH").unwrap());
+            return path
+        } else {
+            return var_os("PATH").unwrap()
+        }
     }
 
-    if matches.opt_present("version") {
-        println!("Rusty which v1.00, Copyright (c) 2019 Graham Ollis.");
-        exit(0);
+    #[cfg(unix)]
+    fn get_path(&self) -> OsString {
+        var_os("PATH").unwrap()
     }
 
-    if matches.free.is_empty() {
-        eprintln!("{}: Too few arguments", program);
-        exit(1);
-    }
+    fn print_usage(&self, opts: Options, message: Option<getopts::Fail>) -> ! {
+        let brief = format!("Usage: {} [options] COMMAND", self.program_name);
 
-    return matches
-}
-
-fn main() {
-    let mut error = 0;
-
-    let m = get_options();
-
-    let cwd = current_dir().unwrap();
-    let path = get_path(m.opt_defined("skip-dot") && m.opt_present("skip-dot"));
-
-    for program_name in m.free {
-
-        match which_in(program_name.clone(), Some(path.clone()), &cwd) {
-            Ok(path) => {
-                let path = path.to_string_lossy();
-                println!("{}", path);
+        match message {
+            Some(message) => {
+                eprintln!("{}: {}", self.program_name, message);
+                eprint!("{}", opts.usage(&brief));
+                exit(1)
             },
-            Err(_) => {
-                eprintln!("{}: Command not found", program_name);
-                error = 1;
+            None => {
+                print!("{}", opts.usage(&brief));
+                exit(0)
             }
         }
     }
 
-    if error == 1 {
-        exit(1);
+    fn get_options(&mut self) {
+        let args: Vec<String> = args().collect();
+        self.program_name = args[0].clone();
+
+        let mut opts = Options::new();
+
+        /* TODO: implement all */
+        opts.optflag("a","all","Print all matches in PATH, not just the first.");
+        opts.optflag("","help", "Print this help and exit successfully.");
+        #[cfg(windows)]
+        opts.optflag("","skip-dot", "Do not search current directory (a la PowerShell)");
+        opts.optflag("v","version", "Print version and exit successfully.");
+
+        let matches = match opts.parse(&args[1..]) {
+            Ok(m)  => m,
+            Err(f) => self.print_usage(opts, Some(f))
+        };
+
+        if matches.opt_present("help") {
+            self.print_usage(opts, None)
+        }
+
+        if matches.opt_present("version") {
+            println!("Rusty which v1.00, Copyright (c) 2019 Graham Ollis.");
+            exit(0)
+        }
+
+        if matches.free.is_empty() {
+            eprintln!("{}: Too few arguments", self.program_name);
+            exit(1)
+        }
+
+        if matches.opt_defined("skip-dot") && matches.opt_present("skip-dot") {
+            self.skip_dot = true
+        }
+
+        self.command_vec = matches.free;
     }
+
+    fn run(&self) -> i32 {
+        let mut rv = 0;
+        let cwd = current_dir().unwrap();
+        let path = self.get_path();
+
+        for program_name in self.command_vec.iter() {
+
+            match which_in(program_name.clone(), Some(path.clone()), &cwd) {
+                Ok(path) => {
+                    let path = path.to_string_lossy();
+                    println!("{}", path);
+                },
+                Err(_) => {
+                    eprintln!("{}: Command not found", program_name);
+                    rv = 1;
+                }
+            }
+        }
+
+        rv
+    }
+
+}
+
+fn main() {
+    let mut app = App::new();
+    app.get_options();
+    let rv = app.run();
+    exit(rv);
 }
