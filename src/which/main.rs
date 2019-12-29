4e5c2d12b93073e8e18c2d8c89a9628d2d3a9841
Graham Ollis <plicease@cpan.org>
2019-12-29 14:17:57 -0700

    search . on windows

--- src/which/main.rs
+++ src/which/main.rs
@@ -1,6 +1,27 @@
-use which::which;
+use which::which_in;
+use std::env::current_dir;
 use std::env::args;
+use std::env::var_os;
 use std::process::exit;
+use std::ffi::OsString;
+
+#[cfg(windows)]
+fn get_path(skip_dot: bool) -> OsString {
+    if !skip_dot {
+        let mut path = OsString::new();
+        path.push(current_dir().unwrap().as_os_str());
+        path.push(";");
+        path.push(var_os("PATH").unwrap());
+        return path
+    } else {
+        return var_os("PATH").unwrap()
+    }
+}
+
+#[cfg(unix)]
+fn get_path(skip_dot: bool) -> OsString {
+    var_os("PATH").unwrap()
+}
 
 fn main() {
     let mut count = 0;
@@ -9,13 +30,16 @@ fn main() {
     /* TODO:
        --version
        --help
-       --skip-dot  /* FIXME currently we don't search . */
+       --skip-dot
        --all | -a (also where)
     */
 
+    let cwd = current_dir().unwrap();
+    let path = get_path(false);
+
     for program_name in args().skip(1) {
 
-        match which(program_name.clone()) {
+        match which_in(program_name.clone(), Some(path.clone()), &cwd) {
             Ok(path) => {
                 let path = path.to_string_lossy();
                 println!("{}", path);
