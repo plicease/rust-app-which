1c2455a5ea77db3706f5b9c9c7666dff2b53b5de
Graham Ollis <plicease@cpan.org>
2019-12-29 08:59:53 -0700

    more complete implementation

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,39 @@ use std::process::exit;
 
 fn main() {
     let mut count = 0;
+    let mut error = 0;
+
+    /* TODO:
+       --version
+       --help
+       --skip-dot
+       --all | -a (also where)
+    */
 
     for program_name in args().skip(1) {
-        let path = which(program_name).unwrap();
-        let path = path.to_string_lossy();
-        println!("{}", path);
-        count += 1;
+
+        let diag_name = program_name.clone();
+
+        match which(program_name) {
+            Ok(path) => {
+                let path = path.to_string_lossy();
+                println!("{}", path);
+                count += 1;
+            },
+            Err(_) => {
+                eprintln!("{}: Command not found", diag_name);
+                error = 1;
+            }
+        }
     }
-    
+
     if count == 0 {
+        /* TODO: this should (maybe) be argv[0] */
+        eprintln!("which: Too few arguments");
+        exit(1);
+    }
+
+    if error == 1 {
         exit(1);
     }
 }
