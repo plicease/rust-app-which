86e9bcdca112e24129d67ddce4c3869a1591d92b
Graham Ollis <plicease@cpan.org>
2019-12-29 15:00:56 -0700

    use standard options parser

--- src/which/main.rs
+++ src/which/main.rs
@@ -4,6 +4,7 @@ use std::env::args;
 use std::env::var_os;
 use std::process::exit;
 use std::ffi::OsString;
+use getopts::Options;
 
 #[cfg(windows)]
 fn get_path(skip_dot: bool) -> OsString {
@@ -23,21 +24,66 @@ fn get_path(skip_dot: bool) -> OsString {
     var_os("PATH").unwrap()
 }
 
+fn print_usage(program: &str, opts: Options, message: Option<getopts::Fail>) -> ! {
+    let brief = format!("Usage: {} [options] COMMAND", program);
+
+    match message {
+        Some(message) => {
+            eprintln!("{}: {}", program, message);
+            eprint!("{}", opts.usage(&brief));
+            exit(1);
+        },
+        None => {
+            print!("{}", opts.usage(&brief));
+            exit(0);
+        }
+    }
+}
+
+fn get_options() -> getopts::Matches {
+    let args: Vec<String> = args().collect();
+    let program = args[0].clone();
+
+    let mut opts = Options::new();
+
+    /* TODO: implement all */
+    opts.optflag("a","all","Print all matches in PATH, not just the first.");
+    opts.optflag("","help", "Print this help and exit successfully.");
+    #[cfg(windows)]
+    opts.optflag("","skip-dot", "Do not search current directory (a la PowerShell)");
+    opts.optflag("v","version", "Print version and exit successfully.");
+
+    let matches = match opts.parse(&args[1..]) {
+        Ok(m)  => m,
+        Err(f) => print_usage(&program, opts, Some(f))
+    };
+
+    if matches.opt_present("help") {
+        print_usage(&program, opts, None);
+    }
+
+    if matches.opt_present("version") {
+        println!("Rusty which v1.00, Copyright (c) 2019 Graham Ollis.");
+        exit(0);
+    }
+
+    if matches.free.is_empty() {
+        eprintln!("{}: Too few arguments", program);
+        exit(1);
+    }
+
+    return matches
+}
+
 fn main() {
-    let mut count = 0;
     let mut error = 0;
 
-    /* TODO:
-       --version
-       --help
-       --skip-dot
-       --all | -a (also where)
-    */
+    let m = get_options();
 
     let cwd = current_dir().unwrap();
-    let path = get_path(false);
+    let path = get_path(m.opt_present("skip-dot"));
 
-    for program_name in args().skip(1) {
+    for program_name in m.free {
 
         match which_in(program_name.clone(), Some(path.clone()), &cwd) {
             Ok(path) => {
@@ -49,14 +95,6 @@ fn main() {
                 error = 1;
             }
         }
-
-        count += 1;
-    }
-
-    if count == 0 {
-        /* TODO: this should (maybe) be using argv[0] instead of 'which' */
-        eprintln!("which: Too few arguments");
-        exit(1);
     }
 
     if error == 1 {
