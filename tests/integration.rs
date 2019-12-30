9e1c672a9f17edb343ad74fbc45edd394b7a0c89
Graham Ollis <plicease@cpan.org>
2019-12-30 03:11:29 -0700

    test for too few arguments

--- tests/integration.rs
+++ tests/integration.rs
@@ -2,8 +2,21 @@
 mod integration {
 
     use assert_cmd::Command;
+    use predicates::str::is_match;
 
     const VERSION: &'static str = env!("CARGO_PKG_VERSION");
+
+    #[test]
+    fn no_arguments() {
+        let mut cmd = Command::cargo_bin("which").unwrap();
+        let assert = cmd
+            .assert();
+        assert
+            .failure()
+            .code(1)
+            .stdout("")
+            .stderr(is_match("which(.exe)?: Too few arguments").unwrap());
+    }
 
     #[test]
     fn minus_version() {
@@ -13,8 +26,8 @@ mod integration {
             .assert();
         assert
             .success()
-            .stdout(format!("Rusty which {}, Copyright (c) 2019 Graham THE Ollis.\n", VERSION));
-        
+            .stdout(format!("Rusty which {}, Copyright (c) 2019 Graham THE Ollis.\n", VERSION))
+            .stderr("");
     }
 
     #[test]
@@ -25,8 +38,8 @@ mod integration {
             .assert();
         assert
             .success()
-            .stdout(format!("Rusty which {}, Copyright (c) 2019 Graham THE Ollis.\n", VERSION));
-        
+            .stdout(format!("Rusty which {}, Copyright (c) 2019 Graham THE Ollis.\n", VERSION))
+            .stderr("");
     }
 
 }
